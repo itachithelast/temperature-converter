a5db30eb3062fb485a240d391380829a89fd3fcc
Hamed Namazi <H.namazi91@gmail.com>
2025-02-10 00:06:32 +0330

    project ready

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
 const units = document.querySelectorAll("a")
 const input = document.querySelector("input")
 const convertBtn = document.querySelector("#convert-btn")
+const output = document.querySelector(".text-success")
 
 
-convertBtn.addEventListener("click",null)
+convertBtn.addEventListener("click",()=>{
+    const result = convert()
+    if (result){
+        output.textContent = result
+    }else{
+        output.textContent = "Kidding right!?"
+    }
+})
 
-input.addEventListener("beforeinput",changeObserver)
+input.addEventListener("input",changeObserver)
 
 units.forEach(unit=>{
     unit.addEventListener("click",e=>handleUnitSelection(e))
@@ -18,11 +26,31 @@ function handleUnitSelection(e){
     changeObserver()
 }
 
+function convert(){
+    const from = cleanedString(document.querySelector("#btn-from").textContent)
+    const to = cleanedString(document.querySelector("#btn-to").textContent)
+    const input = parseFloat(cleanedString(document.querySelector("input").value))
+    if (from==="Fahrenheit" && to === "Celsius"){
+        return `${input} ${from} is ${((input-32)* (5/9)).toFixed(2)} ${to}`
+    }else if (from==="Fahrenheit" && to === "Kelvin"){
+        return `${input} ${from} is ${(((input-32)* (5/9))+273.15).toFixed(2)} ${to}`
+    }else if (from==="Celsius" && to === "Fahrenheit"){
+        return `${input} ${from} is ${((input*9/5) + 32).toFixed(2)} ${to}`
+    }else if (from==="Celsius" && to === "Kelvin"){
+        return `${input} ${from} is ${(input+273.15).toFixed(2)} ${to}`
+    }else if (from==="Kelvin" && to === "Celsius"){
+        return `${input} ${from} is ${(input-273.15).toFixed(2)} ${to}`
+    }else if (from==="Kelvin" && to === "Fahrenheit"){
+        return `${input} ${from} is ${(((input-273.15)* 9/5)+32).toFixed(2)} ${to}`
+    }else{return false}
+}
+
+
 function changeObserver(){
     const from = cleanedString(document.querySelector("#btn-from").textContent)
     const to = cleanedString(document.querySelector("#btn-to").textContent)
     const input = parseFloat(cleanedString(document.querySelector("input").value))
-    if (from!=="From Unit" && to!=="To Unit" && input){
+    if (from!=="From Unit" && to!=="To Unit" && input!==NaN){
         convertBtn.classList.remove("disabled")
     }
 }
